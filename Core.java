4e05ee38dbb2fea3711361c5baf2f1d6f44020eb
TheNonchalantAutist <srdd4@kent.ac.uk>
2019-11-24 14:34:36 +0000

    json scenario
    I think i may have figured out the json format... a little. not sure how to call it yet but ill get there

--- Core.java
+++ Core.java
@@ -1,6 +1,8 @@
 class Core
 {
     public static void main(String[] args) {
-        Player sam = new Player();
+        Player player1 = new Player();
+        System.out.println("BEEP BEEP BEEP...\n...Shit, I'm late again! I need to get dressed...\n");
+        
     }
 }
